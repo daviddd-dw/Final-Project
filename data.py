078899d27db82754bbdad439433fcb2f7dbd53c5
daviddd-dw <1092111649david@gmail.com>
2025-03-08 11:39:36 -0800

    Add files via upload

---
+++ data.py
@@ -0,0 +1,19 @@
+# Create a class of players that has attribute of
+# the player's name, position, age, team and statistics.
+# Author: David Wang
+class Player:
+    def __init__(self, name:str, position:str, age:int, team:str, stats:dict[str,float]):
+        self.name = name
+        self.position = position
+        self.age = age
+        self.team = team
+        self.stats = stats  # Dictionary of player statistics (e.g., {'PPG': 25.3, 'APG': 7.2, 'RPG': 6.1})
+
+    def __repr__(self):
+        return 'Player({}, {}, {}, {},{})'.format(
+                self.name,
+                self.position,
+                self.age,
+                self.team,
+                self.stats,
+            )
