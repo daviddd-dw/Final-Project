c56be653e845e02f8d16bec18d7e0926fa2f7508
daviddd-dw <1092111649david@gmail.com>
2025-03-08 11:39:10 -0800

    Delete fp.py

--- fp.py
+++
@@ -1,9 +0,0 @@
-from pkgutil import get_data
-
-import kaggle
-import pandas
-
-kaggle.api.authenticate()
-
-kaggle.api.dataset_download_files("bryanchungweather/nba-player-stats-dataset-for-the-2023-2024", path=".", unzip =True)
-
